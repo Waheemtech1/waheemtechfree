adb84c747dab72e14eed83def2d38e56e34ee58d
WaheemTech <waheemtech@gmail.com>
2026-05-25 14:45:42 +0100

    Change database connection host from 127.0.0.1 to localhost

--- connection/connect.php
+++ connection/connect.php
@@ -1,12 +1,5 @@
  <?php
-
-$conn = mysqli_connect(
-    "127.0.0.1",
-    "root",
-    "",
-    "waheemtech_db"
-);
-
+$conn = mysqli_connect("localhost", "root", "", "waheemtech_db");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
