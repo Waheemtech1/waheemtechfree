f9070b2b25f94c52d7d98915c30fe8af3d4fd859
WaheemTech <waheemtech@gmail.com>
2026-05-25 10:19:48 +0100

    Update connect.php

--- connection/connect.php
+++ connection/connect.php
@@ -1,5 +1,12 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "waheemtech_db");
+ <?php
+
+$conn = mysqli_connect(
+    "127.0.0.1",
+    "root",
+    "",
+    "waheemtech_db"
+);
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
